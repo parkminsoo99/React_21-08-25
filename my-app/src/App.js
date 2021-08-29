63e5a10560bacc5c98d919c1678120d3d0c97bdd
박민수 <zzangorc99@naver.com>
2021-08-29 11:51:34 +0000

    key값 설정 예제

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 
-import ScrollBox from './ex/ScrollBox.js';
+import MapExample from './ex/MapExample.js';
 
 class App extends Component{
 	render(){
 		return(
-			<div>
-			<ScrollBox ref={(ref) => this.scrollBox = ref}/>
-			<button onClick = {() => this.scrollBox.scrolltoBottom()}>맨 밑으로</button>
-			</div>
+			<MapExample/>
 		);
 	}
 }
